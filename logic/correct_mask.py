22674bb8a7320de75a9a50e3734f63919403e0fb
Zampal94 <lucasjanisch@web.de>
2021-04-13 19:05:14 +0200

    added mask correction

--- logic/correct_mask.py
+++ logic/correct_mask.py
@@ -83,9 +83,9 @@ if __name__ == "__main__":
     label = inference(image, args.nspix, args.niter, args.fdim, args.color_scale, args.pos_scale, args.weight)
     
     segment = image.copy()
-    mask=mask.copy()
+    mask_final=mask.copy()
     image_vec = []
-    for i in range(0,1500):
+    for i in range(0,500):
     
         print(i)
         segment = image.copy()
@@ -100,43 +100,80 @@ if __name__ == "__main__":
         cnts = imutils.grab_contours(cnts)
         oldhorizontal = 0
         oldvertikal = 0
-        lst_intensities = []
-        b=-1
+        bärig = 0
+        bub=0
+        print(len(cnts))
+        masksegment=mask.copy()
         for c in cnts:
-            b+=1
+            
+            #c[:, :, 0] = c[:, :, 0] * 1.01
+            #c[:, :, 1] = c[:, :,  1] * 1.01
             leftmost = tuple(c[c[:,:,0].argmin()][0])
             rightmost = tuple(c[c[:,:,0].argmax()][0])
             topmost = tuple(c[c[:,:,1].argmin()][0])
             bottommost = tuple(c[c[:,:,1].argmax()][0]) 
-            if (topmost[1]!=bottommost[1] and leftmost[0]!=rightmost[0]): 
-                #mask[lst_intensities]=[2,2,2]
-                print(bottommost) 
-                print(leftmost) 
-                print(rightmost) 
-                print(topmost) 
-                M = cv2.moments(c)
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-                r,g,b = mask[cY, cX]
-                print(r,g,b)
-                
-                cimg= np.zeros_like(image.copy())
-                cv2.drawContours(cimg, cnts, b, color=255, thickness=-1)
-                pts = np.where(cimg == 255)
-                #print(pts[0])
-                
-                mask[pts[0],pts[1]] = [55,55,55]
-                #lst_intensities.append(segment[pts[0], pts[1]])
-                #print(lst_intensities)
+            horizontal = rightmost[0]- leftmost[0] 
+            vertikal = bottommost[1]-topmost[1]
+            
 
-                r,g,b = mask[cY, cX]
-                print(r,g,b)
-                
-                horizontal = rightmost[0]- leftmost[0] 
-                vertikal = bottommost[1]-topmost[1]
-                if (horizontal > oldhorizontal or vertikal > oldvertikal):
-                    oldhorizontal = horizontal
-                    oldvertikal = vertikal
+            if (leftmost[0]>=1 and rightmost[0]<=2391 and topmost[1]>=1 and bottommost[1]<=1143):
 
-                    crop_mask = mask[topmost[1]:bottommost[1],leftmost[0]:rightmost[0]]
-                   
+                print("im here")
+                        
+                if (topmost[1]<=(bottommost[1]) and leftmost[0]<=(rightmost[0])):
+                    #mask[lst_intensities]=[2,2,2]
+                    print(bottommost) 
+                    print(leftmost) 
+                    print(rightmost) 
+                    print(topmost) 
+                    M = cv2.moments(c)
+                    if (M["m00"]>0):
+                        cX = int(M["m10"] / M["m00"])
+                        cY = int(M["m01"] / M["m00"])
+                    else:
+                        cX = leftmost[0]
+                        cY= leftmost[1]
+                    r,g,b = mask[cY, cX]
+                    print(r,g,b)
+
+                    if(r==5 and g==5 and b==225):
+                    
+                        cimg= np.zeros_like(image.copy())
+                        cv2.drawContours(cimg, cnts, bub, color=24, thickness=-1)
+                        pts = np.where(cimg == 24)
+                        #print(pts[0])
+                        masksegment[pts[0],pts[1]] = [55,55,55]
+                        add = 2
+                        crop_mask = masksegment[(topmost[1]-add):(bottommost[1]+add),(leftmost[0]-add):(rightmost[0]+add)]
+                        #cv2.imwrite(('/home/janischl/ssn-pytorch/woooooooo0os.png'),crop_mask)
+                        Fault = True
+                        k=0
+                        l=0
+                        if ( np.any(crop_mask==[5,5,225]) or np.any(crop_mask==[255,255,255])):  #
+                            Fault = False
+                        #for k in (range(vertikal-1)):
+                        #    k+=1
+                        #                         
+                        #    for l in (range(horizontal-1)):
+                        #        l=l+1
+                        #        print(crop_mask[k,l,0])
+                        #        if (crop_mask[k,l,0] != 0 & crop_mask[k,l,1] != 0 & crop_mask[k,l,2] != 0 & crop_mask[k,l,0] != 55 & crop_mask[k,l,1] != 55 & crop_mask[k,l,2] != 55 ):               #) and crop_mask[k,l] != [55,55,55]):
+                        #            Fault = False
+                        if (Fault==True):
+                            bärig = 3
+                            print("Fault detected")
+                            mask_final[pts[0],pts[1]] = [0,255,0]
+                            
+                        else:
+                            masksegment[pts[0],pts[1]] = [55,55,55]
+
+                    
+                        #lst_intensities.append(segment[pts[0], pts[1]])
+                        #print(lst_intensities)
+
+                        r,g,b = mask[cY, cX]
+                        print(r,g,b)
+                        cv2.imwrite(('/home/janischl/ssn-pytorch/wooooooooos.png'),mask_final)
+           
+            bub=bub+1    
+            print(bärig)    
